2e931a5acb2a37d3cd06e16069399f7a543d7d44
hejinyu <1144565385@qq.com>
2023-05-21 22:06:59 +0800

    项目的初始化

---
+++ mall-coupon/src/test/java/com/hejinyu/mallcoupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hejinyu.mallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
